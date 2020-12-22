22b28f69bb1de937a80b9fe0b6f908c75c9da5d6
Erik Cooke <erikjulie70@gmail.com>
2020-12-22 13:31:26 -0500

    Created gameboardTest file

--- GameBoard.py
+++ GameBoard.py
@@ -4,11 +4,12 @@
 # creates a game board
 # Author: Erik Cooke
 
-class GameBoard():
+class GameBoard:
 	"""rows-number of rows in game board(max 9)
 	columns-number of columns in game board(max 9)
 	init_char - The character to fill the board with initially"""
-	def __init__(self, rows=3, columns=3,init_char=' '):
+
+	def __init__(self, rows=3, columns=3, init_char=' '):
 		if rows > 9:
 			self.rows = 9
 		else:
@@ -18,28 +19,23 @@ class GameBoard():
 		else:
 			self.columns = columns
 		self.init_char = init_char
-		self.board = [[init_char for i in range(self.columns)]for j in range(self.rows)]
-		
+		self.board = [[init_char for i in range(self.columns)] for j in range(self.rows)]
 
 	def init_board(self):
 		"""Sets the board back to its original state"""
-		self.board = [[self.init_char for i in range(self.columns)]for j in range(self.rows)]
-
+		self.board = [[self.init_char for i in range(self.columns)] for j in range(self.rows)]
 
 	def get_num_rows(self):
 		"""Returns the number of rows"""
 		return self.rows
 
-
 	def get_num_columns(self):
 		"""Returns the number of columns"""
 		return self.columns
 
-
 	def get_row(self, row):
 		"""Returns a list containing each element in row"""
 		return self.board[row]
-
 
 	def get_column(self, column):
 		"""Returns a list containing each element in column"""
@@ -48,11 +44,10 @@ class GameBoard():
 			col.append(self.board[i][column])
 		return col
 
-
 	def get_diagonal(self, row, column, direction="up"):
 		"""Returns a list containing each element in a diagonal direction starting at the row, column going in the designated direction to the right.  Default direction is up"""
 		diagonal = []
-		if direction.lower()=="up":
+		if direction.lower() == "up":
 			while row >= 0 and column < self.columns:
 				diagonal.append(self.board[row][column])
 				row -= 1
@@ -64,28 +59,24 @@ class GameBoard():
 				column += 1
 		return diagonal
 
-
 	def get_position(self, row, column):
 		"""Returns the character stored at the row, column position on the board"""
 		return self.board[row][column]
-
 
 	def set_position(self, player, row, column):
 		"""Places the first character from player into the position row, column on the board"""
 		self.board[row][column] = player[0]
 
-
 	def print_board(self):
 		"""Prints the board to the console with column number headings and lines between rows and columns"""
 		print("")
-		print(" ",end="")
+		print(" ", end="")
 		for i in range(self.columns):
-			print(str(i + 1),end=" ")
+			print(str(i + 1), end=" ")
 		print("")
-		print("-" * (self.columns *2) + "-")
+		print("-" * (self.columns * 2) + "-")
 		for row in self.board:
 			print("|", end="")
 			for col in row:
 				print(col + "|", end="")
-			print("\n" + "-" * (self.columns *2) + "-")
-
+			print("\n" + "-" * (self.columns * 2) + "-")
